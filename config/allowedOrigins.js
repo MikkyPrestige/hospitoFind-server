7e652e7fba50f7f34cebc48ad194b8f78de96dfb
Mikky <meekyberry6@gmail.com>
2023-07-25 03:33:41 +0100

    update gitignore and env files

---
+++ config/allowedOrigins.js
@@ -0,0 +1,11 @@
+// List of allowed origins
+const allowedOrigins = [
+  process.env.SERVER_ORIGIN,
+  process.env.CLIENT_ORIGIN,
+  process.env.AZURE_ORIGIN,
+  process.env.PROD_ORIGIN,
+  process.env.LOCALHOST_API,
+  process.env.LOCALHOST_CLIENT,
+]
+
+export default allowedOrigins;
--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import asyncHandler from "express-async-handler";
 // Connect to the database
 const connectDB = asyncHandler(async () => {
-  await mongoose.connect(process.env.MongoDB_URI, {
+  await mongoose.connect(process.env.MONGODB_URI, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
   });
