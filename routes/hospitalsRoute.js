b812604b25f2618732c3c9e0a83adfb31029f5ec
Mikky <mikkylanky03@gmail.com>
2025-10-12 04:26:13 +0100

    geo updates

--- routes/hospitalsRoute.js
+++ routes/hospitalsRoute.js
@@ -3,33 +3,28 @@ import hospitalController from "../controllers/hospitalController.js";
 
 const hospitalRouter = express.Router();
 
-hospitalRouter.route("/")
+hospitalRouter
+  .route("/")
   .get(hospitalController.getHospitals)
   .post(hospitalController.addHospital)
-  .patch(hospitalController.updateHospital)
+  .patch(hospitalController.updateHospital);
 
-hospitalRouter.route("/random")
-  .get(hospitalController.getRandomHospitals)
+hospitalRouter.route("/random").get(hospitalController.getRandomHospitals);
 
-hospitalRouter.route("/find")
-  .get(hospitalController.findHospitals)
+hospitalRouter.route("/find").get(hospitalController.findHospitals);
 
-hospitalRouter.route("/search")
-  .get(hospitalController.searchHospitals)
+hospitalRouter.route("/search").get(hospitalController.searchHospitals);
 
-hospitalRouter.route("/share")
-  .post(hospitalController.shareHospitals)
+hospitalRouter.route("/share").post(hospitalController.shareHospitals);
 
-hospitalRouter.route("/share/:linkId")
-  .get(hospitalController.getSharedHospitals)
+hospitalRouter
+  .route("/share/:linkId")
+  .get(hospitalController.getSharedHospitals);
 
-hospitalRouter.route("/export")
-  .get(hospitalController.exportHospitals)
+hospitalRouter.route("/export").get(hospitalController.exportHospitals);
 
-hospitalRouter.route("/:name")
-  .get(hospitalController.getHospitalByName)
+hospitalRouter.route("/:name").get(hospitalController.getHospitalByName);
 
-hospitalRouter.route("/")
-  .delete(hospitalController.deleteHospital);
+hospitalRouter.route("/").delete(hospitalController.deleteHospital);
 
 export default hospitalRouter;
