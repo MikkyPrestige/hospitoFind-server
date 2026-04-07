e99adb53f56fa0073d928999d368831f91ddbd12
Mikky <mikkylanky03@gmail.com>
2026-04-07 05:48:54 +0100

    Restrict user listing and role updates to admins

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,31 +1,24 @@
 import express from "express";
 import userController from "../controllers/userController.js";
-import { verifyJWT } from "../middleware/verifyRoles.js";
+import { verifyJWT, verifyAdmin } from "../middleware/verifyRoles.js";
 
 const userRouter = express.Router();
 
 userRouter.use(verifyJWT);
 
-userRouter
-  .route("/")
-  .get(userController.getAllUsers)
-  .patch(userController.updateUser)
-  .delete(userController.deleteUser);
-userRouter.route("/role").patch(userController.updateUserRole);
+// admin-only
+userRouter.route("/").get(verifyAdmin, userController.getAllUsers);
+userRouter.route("/role").patch(verifyAdmin, userController.updateUserRole);
+
+// authenticated user routes
+userRouter.route("/").patch(userController.updateUser).delete(userController.deleteUser);
 userRouter.route("/stats").get(userController.getUserStats);
 userRouter.route("/password").patch(userController.updatePassword);
-// userRouter.route("/favorites").post(userController.toggleFavorite);
 userRouter.route("/view").post(userController.recordView);
 userRouter.route("/activity").get(userController.getUserActivity);
-userRouter
-  .route("/history/:hospitalId")
-  .delete(userController.removeHistoryItem);
+userRouter.route("/history/:hospitalId").delete(userController.removeHistoryItem);
 userRouter.route("/history").delete(userController.clearAllHistory);
-userRouter
-  .route("/favorites-status/:hospitalId")
-  .post(userController.toggleFavoriteStatus);
-userRouter
-  .route("/favorites/:hospitalId")
-  .delete(userController.removeFavorite);
+userRouter.route("/favorites-status/:hospitalId").post(userController.toggleFavoriteStatus);
+userRouter.route("/favorites/:hospitalId").delete(userController.removeFavorite);
 
 export default userRouter;
